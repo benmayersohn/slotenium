ff90caa112312d3810436247c3d3b139acd52a8f
Ben Mayersohn <contact@benmayersohn.com>
2019-06-06 12:41:46 -0400

    Playing with red hot

--- play_game.py
+++ play_game.py
@@ -3,7 +3,7 @@ from helpers import get_url_from_name
 
 # All game names are available in helpers.py
 # We will watch the reels but disable the sound.
-session = IGTSlotSession(get_url_from_name('siberian_storm'), headless=False, sound=False)
+session = IGTSlotSession(get_url_from_name('red_hot'), headless=False, sound=False)
 session.load_game()
 session.spin(num_spins=None)  # run indefinitely until we close the window
 session.save_results()
